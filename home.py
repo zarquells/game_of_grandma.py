fb55d446c421fe8f2f754cd16ecad138e8c514ac
Erica Lelis <erica_lelis@outlook.com>
2025-03-29 22:56:43 -0300

    Após dar uma pesquisada na documentação lógica do numpy, consegui finalizar meu jogo. Incomodada pela quantidade de loops no código, vi alguma função nativa do numpy para substituí-los e isto resolveu metade dos meus problemas.

--- home.py
+++ home.py
@@ -1,37 +1,85 @@
 #  jogo da velha no console com dois players
 #  exercicio de introdução a python
 import numpy as np
+import os
 
-map_grandma = np.array([['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']])
+# limpa a linha de comando Windows
+os.system('cls')
+while True: 
+    map_grandma = np.array([['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']])
 
-while True:
     # player escolhe seu avatar
-    player = input('Bem-vindo ao game of grandma, qual vai ser seu personagem? X ou O?')
+    player = input('\nBem-vindo ao game of grandma, qual vai ser seu personagem? X ou O?\n')
 
-    # valida se é uma das opções
     if player.lower() != 'o' and player.lower() != 'x' and player.upper() != 'O' and player.upper() != 'X':
-        print('Ops, esta não é uma opção válida. Tente novamente.')
+        # valida se é uma das opções de avatar
+        print('\nOps, esta não é uma opção válida. Tente novamente.')
+        continue
 
-    # rastreia cada objeto dentro da matriz (9 objetos na matriz)
-    for i in range(len(map_grandma)):
-        for y in range(len(map_grandma[i])):
-            # pede a jogada do player em relação aquele round (9 rounds : 9 objetos)
-            play = int(input(f'Escolha uma coordenada no mapa: \n {map_grandma[0]} \n {map_grandma[1]} \n {map_grandma[2]} \n'))
-#error: criar validação que previna caso o usuário coloque algo diferente de 'int'
+    # variáveis utilizadas por partida
+    count = 0
+    again = 's'
 
-            # mapeia cada objeto na matriz e compara com a jogada do player
-            for i in range(len(map_grandma)):
-                for y in range(len(map_grandma[i])):
-                    # caso seja correspondente ao objeto encontrado, lança a jogada
-                    if str(play) == map_grandma[i][y]:
-                        map_grandma[i][y] = player.upper()
-#error: criar 'break' para o 'for' quando encontrar a coordenada
-#error: criar validação caso o usuário coloque coordenada já ocupada
+    # inicia uma partida
+    while True:
+        try:
+            # pede a jogada do player
+            play = int(input(f'\nEscolha uma coordenada no mapa: \n {map_grandma[0]} \n {map_grandma[1]} \n {map_grandma[2]} \n'))
+        except Exception:
+            # trata erros caso insira texto, etc.
+            print('\nOps, esta não é uma opção válida. Tente novamente.')
+            continue
 
-            # troca de player
-            if i % 2 == 0 and player.upper() == 'O':
-                player = 'X'
-            elif player.upper() == 'X':
-                player = 'O'
+        if str(play) not in map_grandma:
+            # valida se é um valor existente na matriz
+            print('\nOps, esta não é uma opção válida. Tente novamente.')
+            continue
 
-#update: criar validação de vitória e para o 'while'
+        # localiza e armazena o índice onde este valor é presente
+        ind = np.where(map_grandma == str(play))
+
+        #  substitui pela jogada
+        map_grandma[ind] = player.upper()
+
+        # contabiliza o round
+        count += 1
+
+        if np.any(np.all(map_grandma == player, axis=1)):
+        # verifica se existe uma linha preenchida pelo player do round atual
+            print(f'\n {map_grandma[0]} \n {map_grandma[1]} \n {map_grandma[2]} \nO jogador {player} venceu a partida!')
+            ask = input('\nDeseja jogar novamente? s/n\n')
+            again = ask
+            break
+        elif np.any(np.all(map_grandma == player, axis=0)):
+        # verifica se existe uma coluna preenchida pelo player do round atual
+            print(f'\n {map_grandma[0]} \n {map_grandma[1]} \n {map_grandma[2]} \nO jogador {player} venceu a partida!')
+            ask = input('\nDeseja jogar novamente? s/n\n')
+            again = ask
+            break
+        elif np.any(np.all(np.diag(map_grandma) == player)):
+        # verifica se a diagonal está preenchida pelo player do round atual
+            print(f'\n {map_grandma[0]} \n {map_grandma[1]} \n {map_grandma[2]} \nO jogador {player} venceu a partida!')
+            ask = input('\nDeseja jogar novamente? s/n\n')
+            again = ask
+            break
+        elif count == 9:
+        # caso nenhuma condição seja verdadeira, encerra a partida
+            ask = input('\nJogo Encerrado. Deu velha! \nDeseja jogar novamente? s/n\n')
+            again = ask
+            break
+
+        # troca de player
+        if player.upper() == 'O':
+            player = 'X'
+        elif player.upper() == 'X':
+            player = 'O'
+
+    # verifica se o jogador reiniciou o jogo
+    if again.lower() == 's':
+        continue
+    elif again.lower() == 'n':
+        print('\nObrigada por jogar :D')
+        break
+    else:
+        print('\nOps, esta não é uma opção válida. Mas ainda assim, jogo encerrado :D')
+        break
